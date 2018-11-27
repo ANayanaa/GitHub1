beaf3693770f14460d1892a9e4473961fc184c79
Arnitha <45392215+ANayanaa@users.noreply.github.com>
2018-11-27 09:00:31 -0500

    Method 2 Completed
    Finished the second method where the class prints out the array list without the brackets.

--- arrayList.java
+++ arrayList.java
@@ -11,12 +11,24 @@ public class arrayList{
 			list2.add(i);
 
 		System.out.println(combineList(list1,list2));
+
+			ArrayList <Integer> list3 = new ArrayList<>();
+			for(int i = 50; i<=100; i++)
+			list3.add(i);
+			ArrayList <Integer> list4 = new ArrayList<>();
+				printList(list3);
 		}
 
 		public static  ArrayList<Integer> combineList (ArrayList<Integer>list1, ArrayList<Integer>list2){
 			for(int i = 0; i<list1.size(); i++)
 				list2.add(list1.get(i));
 				return list2;
+			}
+			public static void printList(ArrayList<Integer>list3){
+				for (int i = 0; i<list3.size(); i++)
+					System.out.print(list3.get(i)+", ");
+
+
 			}
 		}
 
